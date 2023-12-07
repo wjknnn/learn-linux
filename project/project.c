226fc506f85d563ce24525f52752693743d464a3
wjknnn <wjknn3123@gmail.com>
2023-12-08 03:11:40 +0900

    [project] sw log print

--- project/project.c
+++ project/project.c
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_STR 1024
 
@@ -17,20 +19,17 @@ struct swInfo {
 }p[10];
 
 int PROCESS_NUM;
+int flag;
+int statusNum;
 
-void readFileList(FILE *fp);
+void readFileList(FILE *fp);		// SW Block 정보 가져오기
 void SwInit(FILE *fp, struct tm *t);
+void SwCheck(int index);
 void countProcess();
+void handler(int signum);		// signal handler
 char *rtrim(const char *s);
 char *ltrim(const char *s);
-char *trim(const char *s);
-
-void countProcess() {
-	for(int i = 0; i < 10; i++) {
-		if(p[i].cnt > 0) PROCESS_NUM++;
-		else break;
-	}
-}
+char *trim(const char *s);		// 공백 제거
 
 int main() {
 	FILE *fp;
@@ -43,37 +42,51 @@ int main() {
 	SwInit(fp, t);
 	countProcess();
 
+	// signal setting
+	struct sigaction act;
+	sigset_t set;
+	sigfillset(&(act.sa_mask));
+	act.sa_handler = handler;
+	sigaction(SIGCHLD, &act, NULL);
+
 	int i;
 	int pnum = 0;
 	pid_t pid[PROCESS_NUM];
 
+	// process 생성
 	pid[0] = fork();
 	for(i = 1; i < PROCESS_NUM; i++) {
 		if(pid[i - 1] > 0) {
 			pid[i] = fork();
-			if(pid[i] > 0) {
-				printf("i'm parent pid[%d]\n", i);
-			}
-			else {
-				printf("i'm child pid[%d]\n", i);
-			}
-			//sleep(1);
+			sleep(1);
 		}
 	}
 	
+	//parent process check
 	for(i = 0; i < PROCESS_NUM; i++) {
 		if(pid[i] > 0) pnum++;
 		else break;
 	}
 
 	if(pnum == PROCESS_NUM) {
-		printf("\n----im parent----\n");
-		for(int i = 0; i < PROCESS_NUM; i++) {
-			printf("%d번째 : %d\n", i, pid[i]);
+		while(1) {
+			if(flag == 1) {
+				sleep(1);
+				timer = time(NULL);
+				t = localtime(&timer);
+				printf("handler is worked [%d:%d:%d]\n", t->tm_hour, t->tm_min, t->tm_sec);
+				break;
+			}
 		}
-		//while(1) {
-			
-		//}
+	}
+}
+
+void handler(int signum) {
+	int pid;
+	int status;
+	while(pid = waitpid(-1, &status, WNOHANG) > 0) {
+		statusNum = status;
+		flag = 1;
 	}
 }
 
@@ -82,39 +95,53 @@ void readFileList(FILE *fp) {
 	char swStr[MAX_STR];
 	int swNum = 0;
 
-	while(fgets(swStr, MAX_STR, fp)) { // 한 줄씩 get
+	while(fgets(swStr, MAX_STR, fp)) { // 한 줄씩 받아오기
 		int n = 0;
 		char *result;
 		swStr[strlen(swStr) - 1] = '\0';	// 개행문자 제거
 		result = strtok(swStr, ";");		// 토큰 파싱
 		strcpy(p[swNum].name, trim(result));	// name 저장
-		printf("\n------%s------\n", p[swNum].name);
+		printf("\n┌──────%3s─────┐\n", p[swNum].name);
 		
 		while(result != NULL && n < 3) {
 			char *result;
 			result = strtok(NULL, ";");
 			if(result != NULL) {
 				strcpy(p[swNum].argv[n], trim(result)); //argv 저장
-				printf("p[%d].argv[%d] = %s\n", swNum, n, p[swNum].argv[n]);
+				printf("│%14s│\n", p[swNum].argv[n]);
 			}
 			else break;
 			n++;
 		}
+		printf("└──────────────┘\n");
 		swNum++;
 	}
+	printf("\n\n");
 	fclose(fp);
 }
 
 void SwInit(FILE *fp, struct tm *t) {
 	fp = fopen("./log/restart.txt", "w");
 
-	for(int i = 0; strlen(p[i].name); i++) {
+	for(int i = 0; strlen(p[i].name); i++) {	
 		sprintf(p[i].time, "%04d.%02d.%02d. %02d:%02d:%02d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
 		p[i].reason = "Init";
 		p[i].cnt++;
 		fprintf(fp, "%s\t%d\t%s\t%s\n", p[i].name, p[i].cnt, p[i].time, p[i].reason);
+		SwCheck(i);
 	}
 	fclose(fp);
+}
+
+void SwCheck(int index) {
+	printf("%s\t%d\t%s\t%s\n", p[index].name, p[index].cnt, p[index].time, p[index].reason);
+}
+
+void countProcess() {
+	for(int i = 0; i < 10; i++) {
+		if(p[i].cnt > 0) PROCESS_NUM++;
+		else break;
+	}
 }
 
 char *rtrim(const char *s) {
